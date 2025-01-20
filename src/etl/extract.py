62880bcc71fab9c9db3a1f182a3656ca0f08387d
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-01-20 14:37:30 +0200

    Add extract data

---
+++ src/etl/extract.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class Extract:
+    def __init__(self):
+        pass
+
+    def extract_data(file_path: str):
+        """
+        Will simply extract the csv data into a dataframe
+        """
+        return pd.read_csv(file_path)
