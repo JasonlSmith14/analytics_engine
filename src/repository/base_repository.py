3e43de2ce9ff235bbdc8968f577bf7899109ba39
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-01-22 10:58:46 +0200

    Add crud many methods

--- src/repository/base_repository.py
+++ src/repository/base_repository.py
@@ -1,5 +1,5 @@
-from typing import Any, Dict
 from sqlalchemy.orm import Session
+from typing import List, Dict, Any
 
 
 class BaseRepository:
@@ -7,9 +7,8 @@ class BaseRepository:
         self.engine = engine
         self.table = table
 
-    def create_single(self, kwargs: Dict[str, Any]):
+    def create_single(self, item):
         with Session(self.engine) as session:
-            item = self.table(**kwargs)
             session.add(item)
             session.commit()
             return item
@@ -37,14 +36,36 @@ class BaseRepository:
                 return True
             return False
 
-    def create_many(self):
-        pass
+    def create_many(self, items: List):
+        with Session(self.engine) as session:
+            session.add_all(items)
+            session.commit()
+            return items
 
-    def read_many(self):
-        pass
+    def read_many(self, filters: Dict[str, Any] = None):
+        with Session(self.engine) as session:
+            query = session.query(self.table)
+            if filters:
+                query = query.filter_by(**filters)
+            return query.all()
 
-    def update_many(self):
-        pass
+    def update_many(self, filters: Dict[str, Any], updates: Dict[str, Any]):
+        with Session(self.engine) as session:
+            items = session.query(self.table).filter_by(**filters).all()
+            if items:
+                for item in items:
+                    for key, value in updates.items():
+                        setattr(item, key, value)
+                session.commit()
+                return True
+            return False
 
-    def delete_many(self):
-        pass
+    def delete_many(self, filters: Dict[str, Any]):
+        with Session(self.engine) as session:
+            items = session.query(self.table).filter_by(**filters).all()
+            if items:
+                for item in items:
+                    session.delete(item)
+                session.commit()
+                return True
+            return False
