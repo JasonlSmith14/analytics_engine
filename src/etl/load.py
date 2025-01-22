8e7938cf9a29067a7289e97409198a16dad30644
JasonlSmith14 <jasonlsmith14@gmail.com>
2025-01-22 10:57:48 +0200

    Add load method which used repository to load all records

--- src/etl/load.py
+++ src/etl/load.py
@@ -1,15 +1,15 @@
+from typing import Any, List
 import pandas as pd
 from sqlalchemy import create_engine
+
+from src.repository.base_repository import BaseRepository
 
 
 class Load:
     def __init__(
         self,
-        db_url: str,
     ):
-        self.db_url = db_url
-        self.engine = create_engine(self.db_url, echo=True)
-        
-    def load(self, data: pd.DataFrame, table_name: str):
-        with self.engine.connect() as conn:
-            data.to_sql(table_name, conn, if_exists="append", index=False)
+        pass
+
+    def load(self, repository: BaseRepository, items: List[Any]):
+        repository.create_many(items)
